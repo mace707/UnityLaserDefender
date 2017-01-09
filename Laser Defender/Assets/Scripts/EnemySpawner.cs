619d5453c4a155302452c9cf111ebd4d80bbc267
Mace <mason@gls.co.za>
2017-01-09 16:01:47 +0200

    Lecture 107

---
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour 
+{
+	[SerializeField]
+	public GameObject EnemyPrefab;
+	[SerializeField]
+	public float Width = 10f;
+	[SerializeField]
+	public float Height = 5f;
+
+	private bool MovingRight = true;
+
+	float XMin = -5;
+	float XMax = 5;
+
+	[SerializeField]
+	float Speed = 5.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		foreach (Transform child in transform) 
+		{
+			GameObject enemy = (GameObject)Instantiate(EnemyPrefab, child.transform.position, Quaternion.identity);
+			enemy.transform.SetParent(child);
+		}
+
+		//Distance between the camera and the object.
+		float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+		XMin = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distanceToCamera)).x ;
+		XMax = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distanceToCamera)).x;
+
+	}
+
+	public void OnDrawGizmos()
+	{
+		Gizmos.DrawWireCube(transform.position, new Vector3(Width, Height));
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		if (MovingRight) transform.position += Vector3.right * Speed * Time.deltaTime;
+		else transform.position += Vector3.left * Speed * Time.deltaTime;
+
+		if (transform.position.x < XMin + Width / 2)
+			MovingRight = true;
+		else if (transform.position.x > XMax - Width / 2)
+			MovingRight = false;
+	}
+}
--- Laser Defender/Assets/Scripts/PlayerScript.cs
+++ Laser Defender/Assets/Scripts/PlayerScript.cs
@@ -10,6 +10,11 @@ public class PlayerScript : MonoBehaviour
 	float XMax = 5;
 	float Padding = 0.5f;
 
+	public GameObject Projectile;
+
+	public float ProjectileSpeed = 0.0f;
+	public float FiringRate = 0.2f;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -21,7 +26,14 @@ public class PlayerScript : MonoBehaviour
 		XMin = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance)).x + Padding;
 		XMax = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distance)).x - Padding;
 	}
-	
+
+	void FireProjectile()
+	{
+		// Quaternion.identity -> means no rotations
+		GameObject beam = (GameObject)Instantiate (Projectile, transform.position, Quaternion.identity);
+		beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, ProjectileSpeed, 0);
+	}
+
 	// Update is called once per frame
 	void Update () 
 	{
@@ -31,6 +43,13 @@ public class PlayerScript : MonoBehaviour
 			transform.position += Vector3.left * Speed * Time.deltaTime;
 		else if (Input.GetKey (KeyCode.RightArrow)) 
 			transform.position += Vector3.right * Speed * Time.deltaTime;
+
+		if (Input.GetKeyDown(KeyCode.Space)) 
+			InvokeRepeating ("FireProjectile", 0.00001f, FiringRate);
+		if (Input.GetKeyUp (KeyCode.Space))
+			CancelInvoke ("FireProjectile");
+
+
 
 		// Restrict the player to the game space.
 		float newX = Mathf.Clamp(transform.position.x, XMin, XMax);
