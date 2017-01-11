e3cd8525cc679aee7a5a77c64b090836efd92416
Mace <mason@gls.co.za>
2017-01-11 13:59:06 +0200

    Lecture 110

---
+++ Laser Defender/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour 
+{
+	[SerializeField]
+	public float Health = 150;
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		Projectile laser = col.gameObject.GetComponent<Projectile> ();
+
+		if(laser)
+		{
+			laser.Hit();
+			Health -= laser.GetDamage();
+			if(Health <= 0)
+				Destroy(gameObject);
+		}
+	}
+}
