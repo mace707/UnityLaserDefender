4a45c996076d6dfcd182e4900a169ab511d67606
Mace707 <masonmossdev@gmail.com>
2017-01-15 20:17:27 +0200

    Lectures Completed.

--- Laser Defender/Assets/Scripts/MusicPlayer.cs
+++ Laser Defender/Assets/Scripts/MusicPlayer.cs
@@ -3,15 +3,52 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
 	static MusicPlayer instance = null;
-	
-	void Start () {
-		if (instance != null && instance != this) {
+
+	public AudioClip StartClip;
+	public AudioClip GameClip;
+	public AudioClip EndClip;
+
+	private AudioSource Music;
+
+	void Start () 
+	{
+		if (instance != null && instance != this) 
+		{
 			Destroy (gameObject);
 			print ("Duplicate music player self-destructing!");
-		} else {
+		} 
+		else 
+		{
 			instance = this;
-			GameObject.DontDestroyOnLoad(gameObject);
+			GameObject.DontDestroyOnLoad (gameObject);
+			Music = GetComponent<AudioSource> ();
+			Music.clip = StartClip;
+			Music.loop = true;
+			Music.Play();
 		}
-		
 	}
+
+	void OnLevelWasLoaded(int level)
+	{
+		Debug.Log("Music Player Loaded Level " + level);
+		Music.Stop();
+
+		switch (level) 
+		{
+		case 0:
+			Music.clip = StartClip;
+			break;
+		case 1:
+			Music.clip = GameClip;
+			break;
+		case 2:
+			Music.clip = EndClip;
+			break;
+		}
+
+		Music.loop = true;
+
+		Music.Play();
+	}
+
 }
