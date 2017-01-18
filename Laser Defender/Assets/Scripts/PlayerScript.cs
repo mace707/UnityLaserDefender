db31a838d79078d477c1f893b914df31fb7d4f80
Mace <mason@gls.co.za>
2017-01-18 15:52:27 +0200

    Added a shield to the player and a double laser gun.

--- Laser Defender/Assets/Scripts/PlayerScript.cs
+++ Laser Defender/Assets/Scripts/PlayerScript.cs
@@ -12,20 +12,32 @@ public class PlayerScript : MonoBehaviour
 	float Padding = 0.5f;
 
 	public GameObject Projectile;
+	public GameObject Shield;
 	public float ProjectileSpeed = 0.0f;
 	public float FiringRate = 0.2f;
-	public float Health = 100;
-	private float MaxHitPoint = 100;
+	public float HitPoints = 100;
+	private float MaxHitPoints = 100;
+	public float ShieldPoints = 100;
+	private float MaxShieldPoints = 100;
+
+
+	private GameObject ActiveShield;
 
 	public AudioClip FireSound;
 	Animator mAnimator;
 	public Image HealthBarForeGround;
-	public Text RatioText;
+	public Text HealthBarRatioText;
+
+	public Image ShieldBarForGround;
+	public Text ShieldBarRatioText;
+
+	[SerializeField]
+	public bool DoubleShotEnabled = true;
 
 	// Use this for initialization
 	void Start () 
 	{
-		MaxHitPoint = Health;
+		MaxHitPoints = HitPoints;
 		//Distance between the camera and the object.
 		float distance = transform.position.z - Camera.main.transform.position.z;
 
@@ -37,24 +49,46 @@ public class PlayerScript : MonoBehaviour
 		mAnimator = GetComponent<Animator>();
 
 		UpdateHealthBar();
+		UpdateShieldPointBar();
 	}
 
 	void FireProjectile()
 	{
 		AudioSource.PlayClipAtPoint(FireSound, transform.position);
 		// Quaternion.identity -> means no rotations
-		GameObject beam = (GameObject)Instantiate (Projectile, transform.position, Quaternion.identity);
-		beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, ProjectileSpeed, 0);
+
+		if(DoubleShotEnabled)
+		{
+			Vector3 leftBeamPos = new Vector3(transform.position.x - 0.5f, transform.position.y + 0.25f, transform.position.z);
+			Vector3 rightBeamPos = new Vector3(transform.position.x + 0.5f, transform.position.y + 0.25f, transform.position.z);
+			GameObject leftBeam = (GameObject)Instantiate(Projectile, leftBeamPos, Quaternion.identity);
+			GameObject rightBeam = (GameObject)Instantiate(Projectile, rightBeamPos, Quaternion.identity);
+			leftBeam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, ProjectileSpeed, 0);
+			rightBeam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, ProjectileSpeed, 0);
+		} 
+		else
+		{
+			GameObject beam = (GameObject)Instantiate(Projectile, transform.position, Quaternion.identity);
+			beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, ProjectileSpeed, 0);
+		}
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
-
 		HandleKeyPress();
 		// Restrict the player to the game space.
 		float newX = Mathf.Clamp(transform.position.x, XMin, XMax);
 		transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
+		if(ActiveShield)
+		{
+			ActiveShield.transform.position = transform.position;
+		} 
+		else
+		{
+			CancelInvoke ("DepleteShield");
+		}
+			
 	}
 
 	void HandleKeyPress()
@@ -82,7 +116,20 @@ public class PlayerScript : MonoBehaviour
 			CancelInvoke ("FireProjectile");
 
 		if(Input.GetKeyDown(KeyCode.LeftShift))
-			Debug.Log("Shield Active");
+		{
+			if(!ActiveShield && ShieldPoints > 0)
+			{
+				ActiveShield = (GameObject)Instantiate(Shield, transform.position, Quaternion.identity);
+				InvokeRepeating("DepleteShield", 0.0001f, 0.1f);
+				CancelInvoke("RegenerateShield");
+			} 
+			else if (ActiveShield)
+			{	
+				Destroy(ActiveShield);
+				InvokeRepeating("RegenerateShield", 5f, 0.5f);
+				CancelInvoke("DepleteShield");
+			}
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D col)
@@ -92,10 +139,34 @@ public class PlayerScript : MonoBehaviour
 		if(laser)
 		{
 			laser.Hit();
-			Health -= laser.GetDamage();
+			HitPoints -= laser.GetDamage();
 			UpdateHealthBar();
-			if(Health <= 0)
+			if(HitPoints <= 0)
 				Die();
+		}
+	}
+
+	void DepleteShield()
+	{
+		ShieldPoints--;
+		UpdateShieldPointBar();
+		if(ShieldPoints <= 0)
+		{
+			Destroy(ActiveShield);
+			CancelInvoke("DepleteShield");
+			ShieldPoints = 0;
+			InvokeRepeating("RegenerateShield", 5f, 0.5f);
+		}
+	}
+
+	void RegenerateShield()
+	{
+		ShieldPoints++;
+		UpdateShieldPointBar();
+		if(ShieldPoints >= MaxShieldPoints)
+		{
+			CancelInvoke("RegenerateShield");
+			ShieldPoints = MaxShieldPoints;
 		}
 	}
 
@@ -106,10 +177,16 @@ public class PlayerScript : MonoBehaviour
 		Destroy(gameObject);
 	}
 
+	private void UpdateShieldPointBar()
+	{
+		float ratio = ShieldPoints / MaxShieldPoints;
+		ShieldBarForGround.rectTransform.localScale = new Vector3(ratio, 1, 1);
+		ShieldBarRatioText.text = "SP " + (Mathf.Floor(ratio * 100)).ToString() + '%';
+	}
 
 	private void UpdateHealthBar()
 	{
-		float ratio = Health / MaxHitPoint;
+		float ratio = HitPoints / MaxHitPoints;
 		HealthBarForeGround.rectTransform.localScale = new Vector3(ratio, 1, 1);
 
 		if(ratio > 0.75)
@@ -121,6 +198,6 @@ public class PlayerScript : MonoBehaviour
 		else if(ratio >= 0)
 			HealthBarForeGround.color = new Color32(255, 0, 0, 200);
 
-		RatioText.text = "HP " + (Mathf.Floor(ratio * 100)).ToString() + '%';
+		HealthBarRatioText.text = "HP " + (Mathf.Floor(ratio * 100)).ToString() + '%';
 	}
 }
---
+++ Laser Defender/Assets/Scripts/PlayerShield.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShield : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{		
+		
+	}
+		
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		Projectile laser = col.gameObject.GetComponent<Projectile>();
+
+		if(laser)
+			laser.Hit();
+	}
+
+}
