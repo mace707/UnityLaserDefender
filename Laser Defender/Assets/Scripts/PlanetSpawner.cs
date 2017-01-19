a675f5890841ad50803d9624461981c94488fba5
Mace <mason@gls.co.za>
2017-01-19 15:52:37 +0200

    Began experimenting with planet spawning

---
+++ Laser Defender/Assets/Scripts/PlanetSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetSpawner : MonoBehaviour 
+{
+	[SerializeField]
+	public GameObject MeteorPrefab;
+
+	public GameObject Planet;
+
+	public void OnDrawGizmos()
+	{
+		Gizmos.DrawWireSphere (transform.position, 1);
+	}
+
+	void SpawnPlanet()
+	{
+		Planet = (GameObject)Instantiate(MeteorPrefab, transform.position, Quaternion.identity);
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!Planet)
+			SpawnPlanet();
+	}
+}
+
