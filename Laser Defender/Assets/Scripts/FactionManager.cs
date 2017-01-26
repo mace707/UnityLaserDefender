bc90827b14a96f84905490dcc7a069aa1dfb860f
Mace <mason@gls.co.za>
2017-01-26 16:27:45 +0200

    Began implementing the faction system.

---
+++ Laser Defender/Assets/Scripts/FactionManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FactionManager : MonoBehaviour 
+{
+	public GameObject[] Factions;
+	// Use this for initialization
+	void Start () 
+	{
+		EnemyScript enemy = Factions[0].transform.GetChild(0).GetChild(0).gameObject.GetComponent<EnemyScript>();
+		if(enemy)
+		{
+			Debug.Log("Found");
+		} 
+		else
+		{
+			Debug.Log("Not Found");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
