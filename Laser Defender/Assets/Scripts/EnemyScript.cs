845a72b48dc567ca52de6d01b1a0979e5266cbb7
Mace <mason@gls.co.za>
2017-01-31 15:58:50 +0200

    Began work on new paths for enemies to take.

--- Laser Defender/Assets/Scripts/EnemyScript.cs
+++ Laser Defender/Assets/Scripts/EnemyScript.cs
@@ -19,11 +19,41 @@ public class EnemyScript : MonoBehaviour
 
 	public GameObject[] ItemDrops; 
 
+	public enum TravelPath
+	{
+		BasicLeftToRight,
+		BasicRightToLeft,
+		LeftToRight2Down1Up,
+		RightToLeft2Down1Up
+	}
+
+	public TravelPath TravelRoute;
+
 	public bool AimedShot = true;
+
+	float XMax = 0;
+	float XMin = 0;
+
+	bool MovingRight = true;
+
+	bool MoveDownStep = false;
+
+	bool MoveUpStep = false;
+
+	float Width, Height;
+
+	public int SpawnCount = 0;
 
 	void Start()
 	{
 		mScoreKeeper = GameObject.Find("Score").GetComponent<ScoreKeeper>();
+
+		float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+		XMin = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distanceToCamera)).x;
+		XMax = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distanceToCamera)).x;
+
+		Width = gameObject.GetComponent<Collider2D>().bounds.size.x;
+		Height = gameObject.GetComponent<Collider2D>().bounds.size.y;
 	}
 
 	void OnTriggerEnter2D(Collider2D col)
@@ -59,6 +89,32 @@ public class EnemyScript : MonoBehaviour
 		{
 			FireProjectile();
 		}
+
+		HandleTravelRoute();
+
+		if(MovingRight)	transform.position += Vector3.right * 2 * Time.deltaTime;
+		else transform.position += Vector3.left * 2 * Time.deltaTime;
+	}
+
+	void HandleTravelRoute()
+	{
+		switch(TravelRoute)
+		{
+		case TravelPath.BasicLeftToRight:
+			MovingRight = true;
+			MoveBasic();
+			break;
+		case TravelPath.BasicRightToLeft:
+			MovingRight = false;
+			MoveBasic();
+			break;
+		case TravelPath.RightToLeft2Down1Up:
+			MoveRightToLeft2down1Up();
+			break;
+		case TravelPath.LeftToRight2Down1Up:
+			MoveLeftToRight2Down1Up();
+			break;
+		}
 	}
 
 	void Die()
@@ -76,4 +132,77 @@ public class EnemyScript : MonoBehaviour
 		Destroy(gameObject);
 	}
 
+	void MoveBasic()
+	{
+		if(transform.position.x < XMin + Width / 2)
+		{
+			MovingRight = true;
+			MoveDownStep = true;
+		} 
+		else if(transform.position.x > XMax - Width / 2)
+		{
+			MovingRight = false;
+			MoveDownStep = true;
+		}
+
+		if(MoveDownStep)
+		{
+			transform.position += Vector3.down * Height;
+			MoveDownStep = false;
+		}
+	}
+
+	void MoveLeftToRight2Down1Up()
+	{
+		if(transform.position.x < XMin + Width / 2)
+		{
+			MovingRight = true;
+			MoveUpStep = true;
+		} 
+		else if(transform.position.x > XMax - Width / 2)
+		{
+			MovingRight = false;
+			MoveDownStep = true;
+		}
+
+		if(MoveDownStep)
+		{
+			transform.position += Vector3.down * (Height * 2);
+			MoveDownStep = false;
+		}
+
+		if(MoveUpStep)
+		{
+			transform.position += Vector3.up * Height;
+			MoveUpStep = false;
+		}
+	}
+
+	void MoveRightToLeft2down1Up()
+	{
+		if(transform.position.x < XMin + Width / 2)
+		{
+			MovingRight = true;
+			MoveUpStep = true;
+		} 
+		else if(transform.position.x > XMax - Width / 2)
+		{
+			MovingRight = false;
+			MoveDownStep = true;
+		}
+
+		if(MoveDownStep)
+		{
+			transform.position += Vector3.down * (Height * 2);
+			MoveDownStep = false;
+		}
+
+		if(MoveUpStep)
+		{
+			transform.position += Vector3.up * Height;
+			MoveUpStep = false;
+		}
+	}
+
 }
+	
---
+++ Laser Defender/Assets/Scripts/EnemySpawnManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnManager : MonoBehaviour 
+{
+	[SerializeField]
+	public FactionManager Factions;
+
+	Vector3 TopLeft, TopRight;
+
+	bool EnemiesSpawned = false;
+
+	int SpawnCount = 0;
+	int FactionIndex = 0;
+	int EnemyIndex = 0;
+	// Use this for initialization
+	void Start () 
+	{
+		float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+		TopLeft = Camera.main.ViewportToWorldPoint (new Vector3 (0, 1, distanceToCamera));
+		TopRight = Camera.main.ViewportToWorldPoint (new Vector3 (1, 1, distanceToCamera));
+		TopLeft += new Vector3(1, -2, 0);
+		TopRight += new Vector3(-1, -2, 0);
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(!EnemiesSpawned)
+		{
+			InvokeRepeating("SpawnNewShip", 1, 1);
+			EnemiesSpawned = true;
+		}
+	}
+
+	void SpawnNewShip()
+	{
+		Transform factionTransform = Factions.Factions[FactionIndex].transform;
+		GameObject enemy =	factionTransform.GetChild(0).GetChild(EnemyIndex).gameObject;
+
+		EnemyScript.TravelPath path = enemy.GetComponent<EnemyScript>().TravelRoute;
+
+		Vector3 startPos = TopLeft;
+
+		if(path == EnemyScript.TravelPath.BasicRightToLeft || path == EnemyScript.TravelPath.RightToLeft2Down1Up)
+			startPos = TopRight;
+
+		Instantiate(enemy, startPos, Quaternion.identity);
+
+		if(SpawnCount > 100)
+		{
+			EnemyIndex++;
+			SpawnCount = 0;
+		}
+		if(EnemyIndex > 3)
+		{
+			EnemyIndex = 0;
+			FactionIndex++;
+		}
+
+		SpawnCount++;
+	}
+}
