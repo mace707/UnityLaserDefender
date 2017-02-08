ffa976def0efaa44173ea45dbb8dfd4bc43cdb85
Mace707 <masonmossdev@gmail.com>
2017-02-08 20:54:16 +0200

    Added Astroid item drops.

---
+++ Laser Defender/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour 
+{
+	// Use this for initialization
+	[SerializeField]
+	public float HitPoints = 0;
+
+	[SerializeField]
+	public GameObject Explosion;
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		Projectile laser = col.gameObject.GetComponent<Projectile> ();
+		if(laser)
+		{
+			laser.Hit();
+			HitPoints -= laser.GetDamage();
+			if(HitPoints <= 0)
+				Die();
+		}
+	}
+
+	void Die()
+	{
+		Instantiate(Explosion, this.transform.position, Quaternion.identity);
+		Destroy(gameObject);
+	}
+
+}
